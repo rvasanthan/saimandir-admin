81311d62af1a89887bd4aa8f393acfa0923aa136
Vasanthan Ravindran <vasanth.ravindran@gmail.com>
2021-09-11 09:52:09 -0400

    Changes made for Heroku

--- server.js
+++ server.js
@@ -1,34 +1,17 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+const { analyzeNgModules } = require('@angular/compiler');
 const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const { join } = require('path');
-const authConfig = require('./auth_config.json');
-
 const app = express();
-
-const port = process.env.SERVER_PORT || 4200;
-
-app.use(morgan('dev'));
-
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      // reportOnly: true,
-      directives: {
-        'default-src': ["'self'"],
-        'connect-src': ["'self'", 'https://*.auth0.com', authConfig.apiUri],
-        'frame-src': ["'self'", 'https://*.auth0.com'],
-        'base-uri': ["'self'"],
-        'block-all-mixed-content': [],
-        'font-src': ["'self'", 'https:', 'data:'],
-        'frame-ancestors': ["'self'"],
-        'img-src': ["'self'", 'data:', '*.gravatar.com'],
-        'style-src': ["'self'", 'https:', "'unsafe-inline'"],
-      },
-    },
-  })
-);
-
-app.use(express.static(join(__dirname, 'dist')));
-
-app.listen(port, () => console.log(`App server listening on port ${port}`));
+//app.use(requireHTTPS);
+app.use(express.static('./dist/saimandir-admin'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/saimandir-admin/'});
+  });
+console.log(process.env.PORT);
+app.listen(process.env.PORT || 8080);
